0a6b01e80f9195e8381f52019ba63b8bb7d876f3
chiunti <chiunti_abc@hotmail.com>
2015-11-29 08:43:17 -0600

    Initial Commit

---
+++ Parse Object/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Parse Object
+//
+//  Created by admin on 29/11/15.
+//  Copyright © 2015 chiuntisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Parse Object/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Parse Object
+//
+//  Created by admin on 29/11/15.
+//  Copyright © 2015 chiuntisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
